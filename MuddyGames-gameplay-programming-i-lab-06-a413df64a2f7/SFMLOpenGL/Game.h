d2b473be796bf8f41d37acfe48b5b385e6c3d95a
(Student) - Jack Nulty <C00273123@itcarlow.ie>
2023-01-23 15:51:43 +0000

    fix progect

--- MuddyGames-gameplay-programming-i-lab-06-a413df64a2f7/SFMLOpenGL/Game.h
+++ MuddyGames-gameplay-programming-i-lab-06-a413df64a2f7/SFMLOpenGL/Game.h
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
+#include <gl/GL.h>
+#include <gl/GLU.h>
 
 using namespace std;
 using namespace sf;
@@ -18,4 +20,15 @@ private:
 	void update();
 	void draw();
 	void unload();
+
+	// // Uncomment for Part 2
+	// // ********************
+	// const int primatives;
+
+	//GLuint index;
+	//Clock clock;
+	//Time elapsed;
+
+	//float rotationAngle = 0.0f;
+	// // ********************
 };
